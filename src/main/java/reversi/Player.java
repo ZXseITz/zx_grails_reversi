291d7e72703f6aacfe5b1dbcdeb0764b72e00153
Claudio Seitz <claudio.seitz@students.fhnw.ch>
2017-05-21 20:19:44 +0200

    Implemented player and round, bot only

---
+++ src/main/java/reversi/Player.java
@@ -0,0 +1,47 @@
+package reversi;
+
+import javax.websocket.Session;
+
+/**
+ * Created by Claudio on 21.05.2017.
+ */
+public class Player {
+    private Session session;
+    private Round round;
+
+    public String getID() {
+        return session.getId();
+    }
+
+    public Player(Session session) {
+        this.session = session;
+    }
+
+    public Round getRound() {
+        return round;
+    }
+
+    public boolean isInRound() {
+        return round != null;
+    }
+
+    public void setRound(Round round) {
+        this.round = round;
+    }
+
+    public void send(String json) {
+        try {
+            session.getBasicRemote().sendText(json);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/reversi/Round.java
@@ -0,0 +1,38 @@
+package reversi;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Claudio on 21.05.2017.
+ */
+public class Round {
+    private static final AtomicInteger counter = new AtomicInteger(1);
+
+    private int id;
+    private Player player;
+    private Token.Color playerColor;
+    private Board board;
+
+    public Round(Player player, Board board, Token.Color color) {
+        this.id = counter.getAndIncrement();
+        this.player = player;
+        this.board = board;
+        this.playerColor = color;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Token.Color getPlayerColor() {
+        return playerColor;
+    }
+}
