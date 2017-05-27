3b5cbcf1f75d24eb3b1ba9edda09a6a136bc445c
Claudio Seitz <claudio.seitz@students.fhnw.ch>
2017-05-27 23:34:29 +0200

    added doc json handler

--- src/main/java/reversi/json/JSONHandler.java
+++ src/main/java/reversi/json/JSONHandler.java
@@ -43,6 +43,13 @@ public abstract class JSONHandler {
 
     // server to client
 
+    /**
+     * Generates a new init json message
+     * @param player color of player
+     * @param placed {white, black} current placed tokens
+     * @param selection selectable tokens
+     * @return json as string
+     */
     public static String buildJsonInit(Token.Color player, int[] placed, Token[] selection) {
         JsonObject data = new JsonObject();
         data.addProperty("color", player.getValue());
@@ -50,7 +57,15 @@ public abstract class JSONHandler {
         data.add("selection", buildTokenArray(selection));
         return buildJson(JSONMessage.SERVER_INIT, data);
     }
-    
+
+    /**
+     * Generates a new place json message for the actor
+     * @param player color of actor
+     * @param placed {white, black} current placed tokens
+     * @param source new placed token
+     * @param changes all tokens who has changed
+     * @return json as string
+     */
     public static String buildJSONPlaceClient(Token.Color player, int[] placed, Token source, Token[] changes) {
         JsonObject data = new JsonObject();
         data.addProperty("color", player.getValue());
@@ -60,6 +75,16 @@ public abstract class JSONHandler {
         return buildJson(JSONMessage.SERVER_PLACE_CLIENT, data);
     }
 
+    /**
+     * Generates a new place json message for the opponent of current action
+     * @param player color of actor
+     * @param placed {white, black} current placed tokens
+     * @param source new placed token
+     * @param changes all tokens who has changed
+     * @param selection selectable tokens for the opponent of current action
+     * @param pass can send auto pass action by client
+     * @return json as string
+     */
     public static String buildJSONPlaceOpponent(Token.Color player, int[] placed, Token source, Token[] changes, Token[] selection, int pass) {
         JsonObject data = new JsonObject();
         data.addProperty("color", player.getValue());
@@ -71,12 +96,24 @@ public abstract class JSONHandler {
         return buildJson(JSONMessage.SERVER_PLACE_OPPONENT, data);
     }
 
+    /**
+     * Generates a new pass json message for the actor
+     * @param player color of actor
+     * @return json as string
+     */
     public static String buildJSONPassClient(Token.Color player) {
         JsonObject data = new JsonObject();
         data.addProperty("color", player.getValue());
         return buildJson(JSONMessage.SERVER_PASS_CLIENT, data);
     }
 
+    /**
+     * Generates a new pass json message for the opponent of current action
+     * @param player color of actor
+     * @param selection selectable tokens for the opponent of current action
+     * @param pass can send auto pass action by client
+     * @return json as string
+     */
     public static String buildJSONPassOpponent(Token.Color player, Token[] selection, int pass) {
         JsonObject data = new JsonObject();
         data.addProperty("color", player.getValue());
@@ -85,12 +122,22 @@ public abstract class JSONHandler {
         return buildJson(JSONMessage.SERVER_PASS_OPPONENT, data);
     }
 
+    /**
+     * Generates a new end message for a player
+     * @param win 1 victory, 0 remis, -1 defeat
+     * @return json as string
+     */
     public static String buildJSONEnd(int win) {
         JsonObject data = new JsonObject();
         data.addProperty("win", win);
         return buildJson(JSONMessage.SERVER_END, data);
     }
 
+    /**
+     * Generates a new error message for a player
+     * @param error error code
+     * @return json as string
+     */
     public static String buildJSONError(int error) {
         JsonObject data = new JsonObject();
         data.addProperty("error", error);
@@ -99,10 +146,20 @@ public abstract class JSONHandler {
 
     // client to server
 
+    /**
+     * Gets the x and y of a message
+     * @param object data of client message
+     * @return {x, y} of selected token
+     */
     public static int[] getXYfromJSON(JsonObject object) {
         return new int[] {object.get("x").getAsInt(), object.get("y").getAsInt()};
     }
 
+    /**
+     * Gets the color and game type of a message
+     * @param object data of client message
+     * @return {color, gameType} of selected token
+     */
     public static int[] getColorTypefromJSON(JsonObject object) {
         return new int[] {object.get("color").getAsInt(), object.get("gameType").getAsInt()};
     }
